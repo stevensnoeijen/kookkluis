e461d45bb4f5b63021c50a87a40f1698c350a089
Snoein <2750503+snoein@users.noreply.github.com>
2026-08-02 14:50:22 +0200

    Update repository reference in Comments component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
     Component.Comments({
       provider: 'giscus',
       options: {
-        repo: 'stevensnoeijen/kookkluis',
+        repo: 'snoein/kookkluis',
         repoId: 'R_kgDOPakuMQ',
         category: 'Announcements',
         categoryId: 'DIC_kwDOPakuMc4Ct8KG',
